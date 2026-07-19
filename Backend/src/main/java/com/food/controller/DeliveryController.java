8aeedfed78bca75beacddc081a410a99a7f815fc
Ishwari Gawande <ishwarigawande702@gmail.com>
2026-07-19 17:23:26 +0530

    Fix bugs

--- Backend/src/main/java/com/food/controller/DeliveryController.java
+++ Backend/src/main/java/com/food/controller/DeliveryController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.food.DTO.DeliveryDTO;
 import com.food.service.DelieveryServiceImpl;
 @RestController
+@RequestMapping("/deliveries")
 public class DeliveryController {
 @Autowired
 private DelieveryServiceImpl deliveryService;
--- Backend/src/main/java/com/food/controller/DeliveryOutcomeController.java
+++ Backend/src/main/java/com/food/controller/DeliveryOutcomeController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.food.DTO.DeliveryOutcomeDTO;
 import com.food.service.DeliveryOutcomeServiceImpl;
 @RestController
+@RequestMapping("/delivery-outcomes")
 public class DeliveryOutcomeController {
 	@Autowired 
 	private DeliveryOutcomeServiceImpl deliveryOutcomeService;
