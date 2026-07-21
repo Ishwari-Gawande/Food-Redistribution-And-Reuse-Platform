8604f27783885dbb9278b706be5398477c6d7435
Ishwari Gawande <ishwarigawande702@gmail.com>
2026-07-21 22:31:10 +0530

    Completed Bean Validation annotations to DTO classes

--- Backend/src/main/java/com/food/DTO/UserDTO.java
+++ Backend/src/main/java/com/food/DTO/UserDTO.java
@@ -1,17 +1,40 @@
 package com.food.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
-	
+	  @NotBlank(message="Name is required")
+	    @Size(max=100)
 		private String name;
+	  
+	   @NotBlank(message="Email is required")
+	    @Email(message="Enter valid email")
 	    private String email;
+	   
+	   @NotBlank(message="Password is required")
+	    @Size(min=6,message="Password must be at least 6 characters")
 	    private String password;
-	    private String phone;
+	   
+	    @NotBlank(message="Phone number is required")
+	    @Pattern(
+	        regexp="^[0-9]{10}$",
+	        message="Enter valid 10-digit phone number")
+        private String phone;
+	    
+	    @NotBlank(message="Account type is required")
 	    private String accountType;
+	    
+	    @NotBlank(message="Address is required")
+	    @Size(max=300)
 	    private String address;
+	    
+	    @NotBlank(message="City is required")
 	    private String city;
 }
