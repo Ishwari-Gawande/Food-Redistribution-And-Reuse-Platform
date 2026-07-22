a72d417f9b403060a329d1ef5d248e0cc02a3f57
Ishwari Gawande <ishwarigawande702@gmail.com>
2026-07-22 21:33:31 +0530

    Implement CustomUserDetailsService for Spring Security authentication

---
+++ Backend/src/main/java/com/food/secuirity/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.food.secuirity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.food.entities.User;
+
+//Spring Security does not understand User entity directly
+//so that we have create this customUserDetails
+public class CustomUserDetails implements UserDetails {
+private final User user;
+	
+	public CustomUserDetails(User user) {
+		this.user=user;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getAccountType()));
+	}
+
+	@Override
+	public @Nullable String getPassword() {
+		return user.getPasswordHash();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+return true;
+	}
+
+	
+}
---
+++ Backend/src/main/java/com/food/secuirity/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.food.secuirity;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.food.entities.User;
+import com.food.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found"));
+
+        return new CustomUserDetails(user);
+	}
+
+
+}
